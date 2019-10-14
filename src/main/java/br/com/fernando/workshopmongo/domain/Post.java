1cf59a4255b43340dc2087178c338bdcd45a49b3
fernandoguide <fernando-guide@hotmail.com>
2019-10-14 13:25:12 -0300

    acrescentando comentarios aos posts

--- src/main/java/br/com/fernando/workshopmongo/domain/Post.java
+++ src/main/java/br/com/fernando/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package br.com.fernando.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.fernando.workshopmongo.dto.AuthorDTO;
+import br.com.fernando.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,4 +108,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
