996453c04b70ec5cce24592b9f556131662bcf7b
fernandoguide <fernando-guide@hotmail.com>
2019-10-14 09:15:00 -0300

    criando usuario com metodo POST

--- src/main/java/br/com/fernando/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fernando/workshopmongo/resources/UserResource.java
@@ -1,14 +1,19 @@
 package br.com.fernando.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.fernando.workshopmongo.domain.User;
 import br.com.fernando.workshopmongo.dto.UserDTO;
@@ -20,8 +25,8 @@ public class UserResource {
 	
 	@Autowired
 	private UserService service;
-	@RequestMapping(method=RequestMethod.GET)
-//	@GetMapping
+//	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 //		converte cada elemento da lista original para um elemento dto
@@ -36,6 +41,16 @@ public class UserResource {
  	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+//	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+//		retorna no headers a localizacao do obj criado 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
